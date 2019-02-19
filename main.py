186f58769b4351a32e56b585427738649fdfdfee
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-02-19 18:59:46 -0500

    Ok yeah you have to make a bot user and THEN you get the token

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ def load_data(filename):
 
 async def run():
     """
-    Where the bot gets started. If you wanted to create an database connection pool or other session for the bot to use,
-    it's recommended that you create it here and pass it to the bot as a kwarg.
+    Where the bot gets started. If you wanted to create an database connection
+    pool or other session for the bot to use, it's recommended that you create
+    it here and pass it to the bot as a kwarg.
     """
 
     config = load_data('config.json')
