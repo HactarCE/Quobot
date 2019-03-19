9b43b3598a8050c11ebe63437470bc54b8faa242
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-03-19 16:35:56 -0400

    Version 1.3.6

--- constants/info.py
+++ constants/info.py
@@ -1,7 +1,7 @@
-DEV = True
+DEV = False
 
 NAME = "Quobot"
-VERSION = "1.3.5" + ("-dev" * DEV)
+VERSION = "1.3.6" + ("-dev" * DEV)
 
 ABOUT_TEXT = f"""\
 {NAME} is an open source Discord bot created using \
